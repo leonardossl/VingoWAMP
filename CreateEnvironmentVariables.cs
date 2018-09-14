291ed67e4d826ed54917d61c819e8c8452ed7979
leonardossl <leonardo_ssl@hotmail.com>
2018-09-14 14:01:38 -0300

    Update CreateEnvironmentVariables.cs

--- CreateEnvironmentVariables.cs
+++ CreateEnvironmentVariables.cs
@@ -5,10 +5,22 @@ public static void Set(string name, string value)
 }
 
 //GET
-string path = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine) + ";c:\\";
+string path = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine) + ";c:\\"; //adicionou direto o c:\
 Environment.SetEnvironmentVariable("Path", path, EnvironmentVariableTarget.Machine);
 
 // Get a single environment variable in this sample LOCALAPPDATA
 var localAppData = System.Environment.GetEnvironmentVariable("LOCALAPPDATA");
 // Get a collection of all the environment variables
 var env = System.Environment.GetEnvironmentVariables(); 
+
+public class Example
+{
+   public static void Main()
+   {
+      // Change the directory to %WINDIR%
+      Environment.CurrentDirectory = Environment.GetEnvironmentVariable("windir");
+      DirectoryInfo info = new DirectoryInfo(".");
+
+      Console.WriteLine("Directory Info:   " + info.FullName);
+   }
+}
