c20c592a667973decb1022002b9ec563451642ce
leonardossl <leonardo_ssl@hotmail.com>
2018-09-14 13:48:25 -0300

    Update CreateEnvironmentVariables.cs

--- CreateEnvironmentVariables.cs
+++ CreateEnvironmentVariables.cs
@@ -7,3 +7,8 @@ public static void Set(string name, string value)
 //GET
 string path = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine) + ";c:\\";
 Environment.SetEnvironmentVariable("Path", path, EnvironmentVariableTarget.Machine);
+
+// Get a single environment variable in this sample LOCALAPPDATA
+var localAppData = System.Environment.GetEnvironmentVariable("LOCALAPPDATA");
+// Get a collection of all the environment variables
+var env = System.Environment.GetEnvironmentVariables(); 
